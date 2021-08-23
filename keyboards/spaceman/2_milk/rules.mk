18211df6dc4663b986803ebdf5af32d10ddd8d68
somepin <79652090+somepin@users.noreply.github.com>
2021-08-23 12:26:19 -0400

    add 2% milk vial fw

--- keyboards/spaceman/2_milk/rules.mk
+++ keyboards/spaceman/2_milk/rules.mk
@@ -9,16 +9,16 @@ MCU = atmega32u4
 #   QMK DFU      qmk-dfu
 #   ATmega32A    bootloadHID
 #   ATmega328P   USBasp
-BOOTLOADER = caterina
+BOOTLOADER = atmel-dfu
 
 # Build Options
 #   comment out to disable the options.
 #
 BOOTMAGIC_ENABLE  = no	# Virtual DIP switch configuration
-MOUSEKEY_ENABLE  = no	# Mouse keys
+MOUSEKEY_ENABLE  = yes	# Mouse keys
 EXTRAKEY_ENABLE  = yes	# Audio control and System control
-CONSOLE_ENABLE  = yes	# Console for debug
-COMMAND_ENABLE  = yes    # Commands for debug and configuration
+CONSOLE_ENABLE  = no	# Console for debug
+COMMAND_ENABLE  = no    # Commands for debug and configuration
 SLEEP_LED_ENABLE  = no  # Breathing sleep LED during USB suspend
 NKRO_ENABLE  = yes		# USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 BACKLIGHT_ENABLE  = no  # Custom backlighting code is used, so this should not be enabled
