56c1af1475fecfb1daad2645f9e39421c7a81bd3
somepin <79652090+somepin@users.noreply.github.com>
2021-12-01 20:11:07 -0500

    enable rgb matrix for bm16s

--- keyboards/kprepublic/bm16s/rules.mk
+++ keyboards/kprepublic/bm16s/rules.mk
@@ -7,8 +7,8 @@ BOOTLOADER = atmel-dfu
 # Build Options
 #   comment out to disable the options.
 #
-BOOTMAGIC_ENABLE = lite     # Enable Bootmagic Lite
-MOUSEKEY_ENABLE  = yes  # Mouse keys
+BOOTMAGIC_ENABLE = lite # Enable Bootmagic Lite
+MOUSEKEY_ENABLE  = no   # Mouse keys
 EXTRAKEY_ENABLE  = yes  # Audio control and System control
 CONSOLE_ENABLE   = no   # Console for debug
 COMMAND_ENABLE   = no   # Commands for debug and configuration
@@ -16,6 +16,6 @@ SLEEP_LED_ENABLE = no   # Breathing sleep LED during USB suspend
 NKRO_ENABLE      = yes  # USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 BACKLIGHT_ENABLE = no   # Enable keyboard backlight functionality
 AUDIO_ENABLE     = no
-RGBLIGHT_ENABLE  = yes
+RGBLIGHT_ENABLE  = no
 
 LAYOUTS = ortho_4x4
