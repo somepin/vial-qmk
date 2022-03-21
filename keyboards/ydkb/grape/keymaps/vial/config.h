8f122b90f4baa6d580bb9e980e975ba3bf87e8aa
somepin <79652090+somepin@users.noreply.github.com>
2022-03-21 11:11:35 -0400

    add grape pcb vial

---
+++ keyboards/ydkb/grape/keymaps/vial/config.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xAF, 0x94, 0x3C, 0xD4, 0x42, 0x70, 0xF6, 0xD8}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
+
+#define VIAL_TAP_DANCE_ENTRIES 4
+#define VIAL_COMBO_ENTRIES 4
+#define NO_ACTION_ONESHOT
